80cf830d3d0ca5ec045f3f5486525687ff1d561b
Connecting69 <austinli823@gmail.com>
2021-09-19 16:42:28 -0400

    c

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [pokemons, setPokemons] = useState();
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    const offset = (page - 1) * 10;
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${offset}`)
+      .then((resp) => resp.json())
+      .then((result) => {
+        setPokemons(result.results);
+        setLoading(false);
+      });
+  }, [page]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <div className='container'>
+        {pokemons ? (
+          pokemons.map((v, i) => (
+            <div className='pokemon button'>
+              <p>{v.name}</p>
+            </div>
+          ))
+        ) : (
+          <p>No pokemon</p>
+        )}
+      </div>
+      <div className='pagination'>
+        <button
+          disabled={page === 1 || loading}
+          className='button'
+          onClick={() => (page === 1 ? 1 : setPage(page - 1))}
         >
-          Learn React
-        </a>
-      </header>
+          Prev
+        </button>
+        <button
+          disabled={loading}
+          className='button'
+          onClick={() => setPage(page + 1)}
+        >
+          Next
+        </button>
+      </div>
+      <p className='page-number'>Page {page}</p>
     </div>
   );
 }
